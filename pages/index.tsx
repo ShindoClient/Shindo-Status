5ca4d5972fa7be6fbb21f7b17ce872d50380d24c
soudanieldias <soudanieldias@hotmail.com>
2025-06-08 12:04:34 -0300

    feat: add landing page
    - Add initial landing page with logo and basic layout

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "2rem" }}>
+      <h1>ShindoClient - Página de Status</h1>
+      <Image
+        src='/assets/logo.png'
+        alt="Minha Imagem"
+        width={300}
+        height={300}
+      />
+    </div>
+  );
+}
