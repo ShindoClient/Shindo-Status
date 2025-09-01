af79d857032a1de7ec0be306b00b5e988ea91b0f
MikiDevAHM <st_git_miki@netstat.com.br>
2025-09-01 20:03:11 +0200

    UPDATED

--- env.d.ts
+++ env.d.ts
@@ -1,11 +1,34 @@
-/// <reference types="nuxt" />
+// https://nuxt.com/docs/guide/directory-structure/plugins#automatic-imports
+/// <reference types="@nuxt/kit" />
+/// <reference types="@nuxt/schema" />
 
-// Permite importar arquivos .vue sem erro
+// Vue 3 type declarations
 declare module '*.vue' {
-    import type { DefineComponent } from 'vue'
-    const component: DefineComponent<{}, {}, any>
-    export default component
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
 }
+
+declare module 'vue' {
+  interface ComponentCustomProperties {
+    $t: (key: string, values?: any) => string
+  }
+}
+
+// Nuxt 3 auto-imports
+declare module '#app' {
+  interface RuntimeNuxtHooks {
+    'app:created': () => void
+  }
+}
+
+declare module '@vue/runtime-core' {
+  interface ComponentCustomProperties {
+    $t: (key: string, values?: any) => string
+  }
+}
+
+export {}
 
 // Tipos extras úteis
 /// <reference types="@vueuse/core" />
