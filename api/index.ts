231bd511f4606807b58f578f5072d7e535cf234a
MikiDevAHM <st_git_miki@netstat.com.br>
2025-09-02 12:38:02 +0200

    UPDATED

--- api/index.ts
+++
@@ -1,9 +0,0 @@
-import { defineEventHandler } from 'h3'
-
-export default defineEventHandler((event) => {
-  // Esta rota nunca deve ser acessada diretamente
-  return {
-    status: 'error',
-    message: 'Rota de API não encontrada'
-  }
-})
