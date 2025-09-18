5ca93868d0db10cad7f51fdb6ee1aff14396a825
MikiDevAHM <st_git_miki@netstat.com.br>
2025-09-18 23:34:56 +0200

    UPDATED

--- app.vue
+++ app.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+import { SpeedInsights } from "@vercel/speed-insights/nuxt"
 import { Analytics } from '@vercel/analytics/nuxt';
 </script>
 
 <template>
+  <SpeedInsights/>
   <Analytics />
   <NuxtPage />
 </template>
